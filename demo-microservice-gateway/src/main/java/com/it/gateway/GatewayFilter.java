e7358c9aa4ae6b5ee6cda9b9f1375d632a2cd7a8
agstar <2541415200@qq.com>
2019-06-21 17:13:44 +0800

    增加了hystrix的基本依赖

---
+++ demo-microservice-gateway/src/main/java/com/it/gateway/GatewayFilter.java
@@ -0,0 +1,13 @@
+package com.it.gateway;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+public class GatewayFilter implements GlobalFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return chain.filter(exchange);
+    }
+}
